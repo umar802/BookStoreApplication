2b7b89b484ce99e0446529253792dcca61b52617
farooq09 <farooqf2021@gmail.com>
2025-05-24 16:35:42 +0530

    This is my first project commit

---
+++ src/test/java/com/SpringFromBasics/BookStoreApi/BookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringFromBasics.BookStoreApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
